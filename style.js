48fbfb252e9949ec5e20aa0bdb1f0cc69b5e0f25
Sohaibislam45 <sohaibislam45@gmail.com>
2025-08-26 17:06:56 +0600

    added navbar in home page

--- style.js
+++ style.js
@@ -1,4 +1,16 @@
+// login button to home page
 document.getElementById('btn').addEventListener('click',function(e){
     e.preventDefault();
-    console.log('clicked')
+    const phoneNo=12345678910;
+    const digit=1234;
+    const inputPhn=document.getElementById('input-phn').value;
+    const inputPhnValue=parseInt(inputPhn);
+    const inputDigit=document.getElementById('input-digit').value;
+    const inputDigitValue=parseInt(inputDigit);
+    if(inputPhnValue===phoneNo && inputDigitValue==digit){
+        window.location.href='/home.html'
+    }
+    else{
+        alert('Invalid Phone No or Digit');
+    }
 })
