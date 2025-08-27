dc34ecde809ac88a21dc3c0d8e461d2078c7bd48
Sohaibislam45 <sohaibislam45@gmail.com>
2025-08-28 01:10:49 +0600

    added all the option section and js toggle

--- styles/home.js
+++ styles/home.js
@@ -17,8 +17,54 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
         alert('Invalid pin number');
         return;
     }
-
-
     const totalAmount=availableBalance+addAmount;
     document.getElementById('available-balance').innerText=totalAmount;
+})
+
+// toggling feature
+document.getElementById('add-btn').addEventListener('click',function(){
+    document.getElementById('cash-out-section').style.display='none';
+    document.getElementById('transfer-money-section').style.display='none';
+    document.getElementById('get-bonus-section').style.display='none';
+    document.getElementById('pay-bill-section').style.display='none';
+    document.getElementById('add-money-section').style.display='block';
+})
+document.getElementById('cash-out-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').style.display='none';
+    document.getElementById('transfer-money-section').style.display='none';
+    document.getElementById('get-bonus-section').style.display='none';
+    document.getElementById('pay-bill-section').style.display='none';
+    document.getElementById('cash-out-section').style.display='block';
+})
+document.getElementById('transfer-money-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').style.display='none';
+    document.getElementById('cash-out-section').style.display='none';
+    document.getElementById('get-bonus-section').style.display='none';
+    document.getElementById('pay-bill-section').style.display='none';
+    document.getElementById('transfer-money-section').style.display='block';
+})
+document.getElementById('get-bonus-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').style.display='none';
+    document.getElementById('cash-out-section').style.display='none';
+    document.getElementById('transfer-money-section').style.display='none';
+    document.getElementById('pay-bill-section').style.display='none';
+    document.getElementById('get-bonus-section').style.display='block';
+})
+document.getElementById('pay-bill-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').style.display='none';
+    document.getElementById('cash-out-section').style.display='none';
+    document.getElementById('transfer-money-section').style.display='none';
+    document.getElementById('get-bonus-section').style.display='none';
+    document.getElementById('pay-bill-section').style.display='block';
+})
+
+
+
+
+
+
+
+// log out button clicked
+document.getElementById('log-out-btn').addEventListener('click',function(){
+    window.location.href='/index.html';
 })
